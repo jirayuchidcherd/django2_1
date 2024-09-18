df075ad2745037345941f59e3734f8ab6b7436dc
jirayuchidcherd <6629011005@cdti.ac.th>
2024-09-18 16:04:37 +0700

    update

--- project1/blogs/views.py
+++ project1/blogs/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Blog
 
 def blogs(request):
-    template = loader.get_template('blogs.html')
-    return HttpResponse(template.render())
+  blogs = Blog.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'blogs': blogs,
+  }
+  return HttpResponse(template.render(context, request))
